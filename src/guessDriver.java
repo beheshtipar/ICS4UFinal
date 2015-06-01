64d93d815489800db210d2b2d0810b8f4958b9c3
Ian Bantoto <lcbantoto@gmail.com>
2015-06-01 14:38:30 -0400

    Swapped over to class+driver system
    Now:
    - displays score inside application instead of in console
    - allows for choosing which ability categories to be tested on (in code,
    needs to be implemented in GUI still)

---
+++ src/guessDriver.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+
+
+public class guessDriver {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		guiGuess_v04 x = new guiGuess_v04(true,true,true);
+	}
+}
