2541e1bc8d31587b0f49e77bb197dcd227a8cdd7
Ian Bantoto <lcbantoto@gmail.com>
2015-06-02 14:35:18 -0400

    Move resource files to lib

--- src/guessDriver.java
+++ src/guessDriver.java
@@ -6,6 +6,6 @@ public class guessDriver {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		guiGuess_v05 x = new guiGuess_v05(true,true,true);
+		guiGuess_v06 x = new guiGuess_v06(true,true,true);
 	}
 }
