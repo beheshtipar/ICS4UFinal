d41c1578f2977bee1c6f1348f1a6f4a613d42e4a
sharpstewie <aghaparsa@gmail.com>
2015-06-09 14:10:38 -0400

    more minor changes
    - fixed layers
    - switched to absolute layout
    - restarted v7 of main gui

--- src/guiGuess_v07.java
+++ src/guiGuess_v07.java
@@ -1,26 +1,13 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-import java.awt.GridBagLayout;
-import java.awt.Dimension;
-
-import javax.swing.JLayeredPane;
-
-import java.awt.GridBagConstraints;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.swing.JLabel;
-
 
 public class guiGuess_v07 extends JFrame {
+
 	private JPanel contentPane;
 
 	/**
@@ -41,38 +28,14 @@ public class guiGuess_v07 extends JFrame {
 
 	/**
 	 * Create the frame.
-	 * @throws IOException 
 	 */
-	public guiGuess_v07() throws IOException {
+	public guiGuess_v07() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 800, 600);
+		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
-		contentPane.setPreferredSize(new Dimension(800, 600));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{0, 0};
-		gbl_contentPane.rowHeights = new int[]{0, 0};
-		gbl_contentPane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{1.0, Double.MIN_VALUE};
-		contentPane.setLayout(gbl_contentPane);
-		
-		JLayeredPane layeredPane = new JLayeredPane();
-		GridBagConstraints gbc = new GridBagConstraints();
-		gbc.fill = GridBagConstraints.BOTH;
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		contentPane.add(layeredPane, gbc);
-		
-		JPanel mainContentPanel = new JPanel();
-		layeredPane.setLayer(mainContentPanel, 1);
-		mainContentPanel.setBounds(0, 0, 800, 600);
-		layeredPane.add(mainContentPanel);
-		
-		BufferedImage BG = ImageIO.read(new File("src/akaliBG.jpg")); 
-		JLabel lblBG = new JLabel(new ImageIcon(BG));
-		lblBG.setBounds(0, 0, 800, 600);
-		layeredPane.setLayer(mainContentPanel, 0);
-		layeredPane.add(lblBG);
 	}
+
 }
