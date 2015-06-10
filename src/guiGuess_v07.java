eb9735e3e07b808c3c9537424fb55a87ea5051a2
sharpstewie <aghaparsa@gmail.com>
2015-06-10 14:47:39 -0400

    more changes to main gui
    added more features
    
    - added life labels
    - improved visibility of timer/score

--- src/guiGuess_v07.java
+++ src/guiGuess_v07.java
@@ -12,6 +12,10 @@ import javax.swing.JPanel;
 import java.awt.Font;
 import javax.swing.JButton;
 import java.awt.SystemColor;
+import java.awt.Color;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.SoftBevelBorder;
 
 
 public class guiGuess_v07 {
@@ -59,24 +63,19 @@ public class guiGuess_v07 {
 		frame.getContentPane().add(layeredPane);
 		
 		BufferedImage BG = ImageIO.read(new File("lib//akaliBG.jpg"));
-		JLabel lblBG = new JLabel(new ImageIcon(BG));
+		JLabel lblBG = new JLabel(/*new ImageIcon(BG)*/);
 		layeredPane.setLayer(lblBG, 0);
 		lblBG.setBounds(0, 0, 800, 578);
 		layeredPane.add(lblBG);
 		
 		JPanel mainPane = new JPanel();
-		layeredPane.setLayer(mainPane, 1);
+		mainPane.setForeground(new Color(255, 255, 255));
+		layeredPane.setLayer(mainPane, 2);
 		mainPane.setBounds(0, 0, 800, 578);
 		layeredPane.add(mainPane);
 		mainPane.setBackground(null);
 		mainPane.setOpaque(false);
 		mainPane.setLayout(null);
-		
-		JLabel lblTitle = new JLabel("Guess That Champion!");
-		lblTitle.setForeground(SystemColor.desktop);
-		lblTitle.setFont(new Font("Baskerville", Font.BOLD, 16));
-		lblTitle.setBounds(311, 25, 177, 20);
-		mainPane.add(lblTitle);
 		
 		JLabel lblQuizPic = new JLabel("");
 		lblQuizPic.setBounds(368, 102, 64, 64);
@@ -97,6 +96,37 @@ public class guiGuess_v07 {
 		JButton btnChamp4 = new JButton("");
 		btnChamp4.setBounds(408, 320, 120, 120);
 		mainPane.add(btnChamp4);
+		
+		JLabel lblScore = new JLabel("Score: 200\n");
+		lblScore.setFont(new Font("Bangla MN", Font.BOLD, 13));
+		lblScore.setForeground(new Color(255, 255, 255));
+		lblScore.setBounds(351, 470, 113, 25);
+		mainPane.add(lblScore);
+		
+		JLabel lblTimer = new JLabel("59");
+		lblTimer.setFont(new Font("Lucida Grande", Font.PLAIN, 21));
+		lblTimer.setForeground(new Color(255, 255, 255));
+		lblTimer.setBounds(384, 512, 32, 37);
+		mainPane.add(lblTimer);
+		
+		JLabel lblLife1 = new JLabel("");
+		lblLife1.setBounds(68, 200, 64, 64);
+		mainPane.add(lblLife1);
+		
+		JLabel lblLife2 = new JLabel("\n");
+		lblLife2.setBounds(68, 274, 64, 64);
+		mainPane.add(lblLife2);
+		
+		JLabel lblLife3 = new JLabel("");
+		lblLife3.setBounds(68, 348, 64, 64);
+		mainPane.add(lblLife3);
+		
+		JPanel timerBG = new JPanel();
+		timerBG.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		timerBG.setBackground(new Color(0, 0, 0));
+		layeredPane.setLayer(timerBG, 1);
+		timerBG.setBounds(300, 460, 200, 100);
+		layeredPane.add(timerBG);
 		
 		
 //		BufferedImage BG = ImageIO.read(new File("src/akaliBG.jpg"));
